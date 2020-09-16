1899ff8cf7cb02c9aa6e4f944ec895fa9d581eb3
hamzagh1998 <bmca.1920@gmail.com>
2020-09-16 22:11:13 +0100

    enhance /chat router

--- routes/message-box.js
+++ routes/message-box.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 const router = require('express').Router()
 
 const { verifyUser } = require('../middlewares/jwtAuth')
-// Helper function
+// Helper functions
 const checkPeer = require('../helpers/checkPeer')
 const createChatBox = require('../helpers/createChatBox')
 
@@ -10,16 +10,19 @@ router.post('/chat', verifyUser, async (req, res) => {
   // Check if the peer id isn't the same is of the user sender
   if (req.user.id.toString() === req.body.peerId) return res.sendStatus(403) // Forbidden
 
-  let senderChatDoc = null
+  let response = null
   const messageObj = {user: req.user, message: req.body.message}
-  const peer = await User.findById({_id: req.body.peerId})
-  const chatBox  = await checkPeer(req.user.id, peer._id, messageObj) // Check if chat box already exits
-  if (chatBox) { // If chat box Already exists
-    senderChatDoc = chatBox
-  } else { // New chatbox doc for the sender
-    senderChatDoc = await createChatBox(req.user, peer, messageObj)
+  try {
+    const peer = await User.findById({_id: req.body.peerId})
+    // Check if chat box already exits
+    const chatBox  = await checkPeer(req.user.id, peer._id, messageObj) 
+    // Update if chat box Already exists OR Crete New chatbox doc for the sender
+    response = chatBox ? chatBox : await createChatBox(req.user, peer, messageObj)
+  } catch (err) {
+    response = {error: true, msg: "Peer not found"}
   }
-  res.json(senderChatDoc)
+
+  res.json(response)
 })
 
 module.exports = router
